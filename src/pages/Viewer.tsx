91090555ff3d6cdf0437e8452ae962856ad34188
simon-nigut <simon.nigut@sens-vue.com>
2025-09-24 08:34:45 +0200

    Added zoom and pan tools

--- src/pages/Viewer.tsx
+++ src/pages/Viewer.tsx
@@ -2,15 +2,15 @@ import ImageInput from "@/components/ImageInput";
 import { useEffectOnce } from "@/lib/useEffectOnce";
 import { useRef } from "react";
 import {
-	init,
+	init as coreInit,
 	RenderingEngine,
 	imageLoader,
 	metaData,
 	getRenderingEngine,
-  Enums,
+	Enums as CoreEnums,
 } from "@cornerstonejs/core";
 import type { Types } from "@cornerstonejs/core";
-import { ToolGroupManager } from "@cornerstonejs/tools";
+import { addTool, init as cornerstoneToolsInit, ToolGroupManager, WindowLevelTool, ZoomTool, Enums as ToolsEnums } from "@cornerstonejs/tools";
 import registerWebImageLoader from "../cornerstone/loaders/registerWebImageLoader";
 import hardcodedMetaDataProvider from "../cornerstone/hardcodedMetaDataProvider";
 
@@ -23,13 +23,17 @@ function Viewer() {
 
   useEffectOnce(() => {
 		const runDemo = async () => {
-			await init();
+			await coreInit();
+			await cornerstoneToolsInit();
 			registerWebImageLoader(imageLoader);
 
 			metaData.addProvider(
 				(type, imageId) => hardcodedMetaDataProvider(type, imageId, []),
 				10000,
 			);
+
+			addTool(ZoomTool);
+			addTool(WindowLevelTool);
 
 			const renderingEngine = new RenderingEngine(renderingEngineId);
 
@@ -39,21 +43,40 @@ function Viewer() {
 				toolGroup = ToolGroupManager.createToolGroup(toolGroupId);
 			}
 
-      if (!toolGroup) {
-        console.error("Failed to create or get tool group");
-        return;
-      }
+			if (!toolGroup) {
+				console.error("Failed to create or get tool group");
+				return;
+			}
+
+			toolGroup.addTool(ZoomTool.toolName);
+			toolGroup.addTool(WindowLevelTool.toolName);
 			toolGroup.addViewport("COLOR_STACK", renderingEngineId);
 
 			const viewportInputArray: Types.PublicViewportInput[] = [
 				{
 					viewportId: "COLOR_STACK",
-					type: Enums.ViewportType.STACK,
+					type: CoreEnums.ViewportType.STACK,
 					element: element1Ref.current!,
 				},
 			];
 
 			renderingEngine.setViewports(viewportInputArray);
+
+			toolGroup.setToolActive(WindowLevelTool.toolName, {
+			bindings: [
+				  {
+					mouseButton: ToolsEnums.MouseBindings.Primary, // Left Click
+				  },
+				],
+			});
+			  
+			toolGroup.setToolActive(ZoomTool.toolName, {
+			bindings: [
+				{
+				mouseButton: ToolsEnums.MouseBindings.Wheel, // Right Click
+				},
+			],
+			});
 		};
 
 		runDemo();
