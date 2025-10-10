ed1c969ea2b32e398679ae4dd6fde0cb7a99b418
simon-nigut <simon.nigut@sens-vue.com>
2025-10-10 15:21:43 +0200

    hotfix: light theme as default

--- src/App.tsx
+++ src/App.tsx
@@ -4,26 +4,29 @@ import Demo from "./pages/Demo";
 import LoginPage from "./pages/Login";
 import PrivateRoute from "./components/PrivateRoute";
 import { AuthProvider } from "./lib/authContext";
+import { ThemeProvider } from "./components/theme-provider";
 
 function App() {
   return (
     <AuthProvider>
-      {/* Main content */}
-      <main className="flex-1 overflow-y-auto p-0 h-screen w-screen">
-        <Routes>
-          <Route
-            path="/"
-            element={
-              <PrivateRoute>
-                <Viewer />
-              </PrivateRoute>
-            }
-          />
-          <Route path="/login" element={<LoginPage />} />
-          <Route path="/about" element={<h1>About Page</h1>} />
-          <Route path="/demo" element={<Demo />} />
-        </Routes>
-      </main>
+      <ThemeProvider>
+        {/* Main content */}
+        <main className="flex-1 overflow-y-auto p-0 h-screen w-screen">
+          <Routes>
+            <Route
+              path="/"
+              element={
+                <PrivateRoute>
+                  <Viewer />
+                </PrivateRoute>
+              }
+            />
+            <Route path="/login" element={<LoginPage />} />
+            <Route path="/about" element={<h1>About Page</h1>} />
+            <Route path="/demo" element={<Demo />} />
+          </Routes>
+        </main>
+      </ThemeProvider>
     </AuthProvider>
   );
 }
